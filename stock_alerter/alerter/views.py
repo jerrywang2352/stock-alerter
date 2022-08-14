797d4a5f10cb42493f3e32d7648d06c63a53a7a8
Jerry Wang <jerry.wang2352@gmail.com>
2022-08-13 20:20:57 -0400

    Create home page

--- stock_alerter/alerter/views.py
+++ stock_alerter/alerter/views.py
@@ -1,3 +1,8 @@
+from xmlrpc.client import _HostType
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("home")
